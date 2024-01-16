7f0de9bff9c3ccdaecc3778aae3425582d972f0d
Mark M <chtozamm@gmail.com>
2024-01-16 20:32:43 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Counter from "./Counter";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center">
+    <main className="mx-auto flex h-screen w-screen max-w-md flex-col items-center justify-center">
       <Counter />
     </main>
   );
